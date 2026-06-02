43a5d7b410f07e9bfedf72df600a8cbafa947847
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2026-06-02 18:34:24 +0530

    Default site theme to light

--- astro.config.mjs
+++ astro.config.mjs
@@ -96,6 +96,7 @@ export default defineConfig({
       components: {
         SiteTitle: './src/components/ui/starlight/SiteTitle.astro',
         Head: './src/components/ui/starlight/Head.astro',
+        ThemeProvider: './src/components/ui/starlight/ThemeProvider.astro',
         MobileMenuFooter:
           './src/components/ui/starlight/MobileMenuFooter.astro',
         ThemeSelect: './src/components/ui/starlight/ThemeSelect.astro',
